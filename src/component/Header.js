d3e9a0c39eb06228b442bf53e5f81c4d61cd29fc
kdilot <kdilot@gmail.com>
2018-07-26 19:48:39 -0700

    header

---
+++ src/component/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import oc from 'open-color';
+
+const Wrapper = styled.div`
+    /* layout */
+    height: 4rem;
+    background: ${oc.teal[6]};
+    border-bottom: 1px solid ${oc.teal[8]};
+    /* fornt */
+    color: white;
+    font-weight: 500;
+    font-size: 1.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Header = () => {
+    return (
+        <Wrapper>
+            Contact
+        </Wrapper>
+    );
+};
+
+export default Header;
