599e2ff0397f69e532bc4fd7f9ef3f20697eaee3
kdilot <kdilot@gmail.com>
2018-07-30 18:35:42 -0700

    redux modules

---
+++ src/modules/contacts.js
@@ -0,0 +1,71 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Map, List } from 'immutable';
+
+const CREATE = 'contact/CREATE';
+const MODIFY = 'contact/MODIFY';
+const REMOVE = 'contact/REMOVE';
+const TOGGLE_FAVORITE = 'contact/TOGGLE_FAVORITE';
+
+export const create = createAction(CREATE); //  { id, name, phone, color }
+export const modify = createAction(MODIFY); //  { id, contact: { name, phone } }
+export const remove = createAction(REMOVE); // id
+export const toggleFavorite = createAction(TOGGLE_FAVORITE);  // id
+
+const initialState = List([
+  Map({
+    "id": "SyKw5cyAl",
+    "name": "KMJ",
+    "phone": "010-0000-0000",
+    "color": "#40c057",
+    "favorite": true
+  }),
+  Map({
+    "id": "r1s_9c10l",
+    "name": "AVET",
+    "phone": "010-0000-0001",
+    "color": "#12b886",
+    "favorite": true
+  }),
+  Map({
+    "id": "BJcFqc10l",
+    "name": "VETTY",
+    "phone": "010-0000-0002",
+    "color": "#fd7e14",
+    "favorite": false
+  }),
+  Map({
+    "id": "BJUcqqk0l",
+    "name": "CH",
+    "phone": "010-0000-0003",
+    "color": "#15aabf",
+    "favorite": false
+  }),
+  Map({
+    "id": "rJHoq91Cl",
+    "name": "DAVID",
+    "phone": "010-0000-0004",
+    "color": "#e64980",
+    "favorite": false
+  })
+]);
+
+export default handleActions({
+  [CREATE]: (state, action) => {
+    return state.push(Map(action.payload));
+  },
+  [MODIFY]: (state, action) => {
+    const index = state.findIndex(contact => contact.get('id') === action.payload.id);
+
+    return state.mergeIn([index], action.payload.contact);
+  },
+  [REMOVE]: (state, action) => {
+    const index = state.findIndex(contact => contact.get('id') === action.payload);
+
+    return state.delete(index);
+  },
+  [TOGGLE_FAVORITE]: (state, action) => {
+    const index = state.findIndex(contact => contact.get('id') === action.payload);
+
+    return state.update(index, contact => contact.set('favorite', !contact.get('favorite')));
+  }
+}, initialState);
